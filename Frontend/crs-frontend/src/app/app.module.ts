4d2fe2c6e83248d97ec17f947a45e83bcb887412
Rameshwar Waghmare <waghrameshwar123@gmail.com>
2022-05-07 10:23:58 +0530

    changes

--- Frontend/crs-frontend/src/app/app.module.ts
+++ Frontend/crs-frontend/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { AdminComponent } from './components/admin/admin.component';
+import { CustomersComponent } from './components/customers/customers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    CustomersComponent
+>>>>>>> 0b773cda4ca555d6697b56ea82552301200abac1
   ],
   imports: [
     BrowserModule,
