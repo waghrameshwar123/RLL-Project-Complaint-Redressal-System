36c3f92f643bd4ddf718d270686858b34b989112
Rameshwar Waghmare <waghrameshwar123@gmail.com>
2022-05-11 16:58:22 +0530

    Front-end

--- Backend/ComplaintRedressalSystem/src/main/java/com/Application.java
+++ Backend/ComplaintRedressalSystem/src/main/java/com/Application.java
@@ -2,8 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.model")
+@EnableJpaRepositories(basePackages = "com.repository")
 public class Application {
 
 	public static void main(String[] args) {
